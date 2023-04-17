da6377b824f6d7a8f94e9c652c73cda11cbfedd0
secsilm <secsilm@outlook.com>
2023-04-17 22:30:49 +0800

    feat: support downloading graph as interactive html; show the graph using svg

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,16 +6,23 @@ from loguru import logger
 
 import utils
 
-st.title('Generate Python Call Graph Online')
+st.title("Generate Python Call Graph Online")
 
-st.write('Input your python file content, click Generate and wait a minute, you will see the call graph.')
-code = st.text_area(label='Code', placeholder='Please input your Python code here')
+st.write(
+    "Input your python file content, click Generate and wait a minute, you will see the call graph. You can also download it as an interactive html."
+)
+code = st.text_area(label="Code", placeholder="Please input your Python code here")
 clicked = st.button("Generate")
 if clicked:
-    with NamedTemporaryFile(mode='w+', encoding='utf8') as f:
+    with NamedTemporaryFile(mode="w+", encoding="utf8") as f:
         logger.debug(f"{code=}")
         f.write(code)
         f.seek(0)
-        dot = utils.generate_call_graph(f.name)
-    logger.info(f"{dot=}")
-    st.graphviz_chart(dot)
+        data = utils.generate_call_graph(f.name, format="svg")
+        html = utils.generate_call_graph(f.name, format="html")
+    # logger.info(f"{dot=}")
+    # st.graphviz_chart(dot)
+    st.image(data)
+    st.download_button(
+        "Download interactive html", html, file_name="python-cg.html", mime="text/html"
+    )
--- utils.py
+++ utils.py
@@ -1,19 +1,26 @@
+import base64
 import os
 import subprocess
 from pathlib import Path
 from tempfile import NamedTemporaryFile
+import streamlit as st
 
 from loguru import logger
 
 
-def generate_call_graph(pyfile: str, **kwargs) -> str:
+def generate_call_graph(pyfile: str, format='dot', **kwargs) -> str:
     with NamedTemporaryFile(mode='w+', encoding='utf8') as f:
         logger.debug(f"input content: {Path(pyfile).read_text()}")
         logger.debug(f"{f.name=}")
-        # cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--annotated', '--dot', f'>{f.name}']
-        cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--dot', f'>{f.name}']
+        if format == 'dot':
+            # cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--annotated', '--dot', f'>{f.name}']
+            cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--dot', f'>{f.name}']
+            # subprocess.run(cmd, check=True)
+        elif format == 'svg':
+            cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--svg', f'>{f.name}']
+        elif format == 'html':
+            cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--html', f'>{f.name}']
         logger.debug(f"cmd={' '.join(cmd)}")
-        # subprocess.run(cmd, check=True)
         os.system(' '.join(cmd))
         f.seek(0)
         content = f.read()
