cc50f06361642b6407e29f660b3e64f199b1e8d1
secsilm <secsilm@outlook.com>
2023-04-18 17:49:27 +0800

    ffeat: add more options

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,19 +7,30 @@ from loguru import logger
 import utils
 
 st.title("Generate Python Call Graph Online")
-
 st.write(
     "Input your python file content, click Generate and wait a minute, you will see the call graph. You can also download it as an interactive html."
 )
+
 code = st.text_area(label="Code", placeholder="Please input your Python code here")
+with st.sidebar:
+    uses = st.checkbox("Add edges for 'uses' relationships", value=True)
+    defines = st.checkbox("Add edges for 'defines' relationships", value=False)
+    grouped = st.checkbox(
+        "Group nodes (create subgraphs) according to namespace", value=True
+    )
+    colored = st.checkbox("Color nodes according to namespace", value=True)
 clicked = st.button("Generate")
 if clicked:
     with NamedTemporaryFile(mode="w+", encoding="utf8") as f:
         logger.debug(f"{code=}")
         f.write(code)
         f.seek(0)
-        data = utils.generate_call_graph(f.name, format="svg")
-        html = utils.generate_call_graph(f.name, format="html")
+        data = utils.generate_call_graph(
+            f.name, format="svg", defines=defines, grouped=grouped, colored=colored
+        )
+        html = utils.generate_call_graph(
+            f.name, format="html", defines=defines, grouped=grouped, colored=colored
+        )
     # logger.info(f"{dot=}")
     # st.graphviz_chart(dot)
     st.image(data)
--- utils.py
+++ utils.py
@@ -3,26 +3,39 @@ import os
 import subprocess
 from pathlib import Path
 from tempfile import NamedTemporaryFile
-import streamlit as st
 
+import streamlit as st
 from loguru import logger
 
 
-def generate_call_graph(pyfile: str, format='dot', **kwargs) -> str:
-    with NamedTemporaryFile(mode='w+', encoding='utf8') as f:
-        logger.debug(f"input content: {Path(pyfile).read_text()}")
+def generate_call_graph(
+    pyfile: str, format="dot", uses=True, defines=False, grouped=True, colored=True
+) -> str:
+    with NamedTemporaryFile(mode="w+", encoding="utf8") as f:
+        # logger.debug(f"input content: {Path(pyfile).read_text()}")
         logger.debug(f"{f.name=}")
-        if format == 'dot':
+        cmd = ["pyan3", pyfile]
+        cmd.append("--uses" if uses else "--no-uses")
+        cmd.append("--defines" if defines else "--no-defines")
+        if grouped:
+            cmd.append("--grouped")
+        if colored:
+            cmd.append("--colored")
+        if format == "dot":
             # cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--annotated', '--dot', f'>{f.name}']
-            cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--dot', f'>{f.name}']
+            # cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--dot', f'>{f.name}']
             # subprocess.run(cmd, check=True)
-        elif format == 'svg':
-            cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--svg', f'>{f.name}']
-        elif format == 'html':
-            cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--html', f'>{f.name}']
+            cmd.append("--dot")
+        elif format == "svg":
+            # cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--svg', f'>{f.name}']
+            cmd.append("--svg")
+        elif format == "html":
+            cmd.append("--html")
+            # cmd = ['pyan3', pyfile, '--uses', '--no-defines', '--colored', '--grouped', '--html', f'>{f.name}']
+        cmd.append(f">{f.name}")
         logger.debug(f"cmd={' '.join(cmd)}")
-        os.system(' '.join(cmd))
+        os.system(" ".join(cmd))
         f.seek(0)
         content = f.read()
-        logger.debug(f"{content=}")
+        # logger.debug(f"{content=}")
         return content
